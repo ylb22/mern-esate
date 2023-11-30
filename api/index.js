7496ec20b997436150c927cf821d8a7139922144
ylb22 <bhoyeyogesh0202@gmail.com>
2023-11-30 20:18:56 +0530

    create test api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+import { router as userRouter } from "./routes/user.route.js";
 
 mongoose
   .connect(process.env.MONGO)
@@ -12,6 +13,8 @@ mongoose
     console.log(err);
   });
 const app = express();
+
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
   console.log("Server is running port 3000!");
